5ab65afbddf6242e54d553baaf6e9f3ffe29497b
dpnraju95 <dpnraju95@gmail.com>
2021-12-21 12:33:49 +0530

    final Changes

--- Assignment-3/code/src/App.js
+++ Assignment-3/code/src/App.js
@@ -7,16 +7,16 @@ var arrow="<";
 var arrow2=">";
 
 const profile_details={
-  "Name":"Tamanna Garg",
-  "EmployeeID":"00002512",
-  "Appointment":"9:00(24-05-2021)",
-  "Email":"tamanna@gmail.com",
-  "Phone":"9876543210"
+  "Name":"Datla Pardhu Narasimha Raju",
+  "EmployeeID":"18111995",
+  "Appointment":"11:00(24-12-2021)",
+  "Email":"dpnraju95@gmail.com",
+  "Phone":"1234567890"
 }
 const order_detail={
   "Status": "In Progress",
   "Door" : "Mark",
-  "Time":"10:30(25-05-2021)",
+  "Time":"11:00(26-12-2021)",
 }
 
 function App() {
