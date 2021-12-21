dc3fdead14b386542e1ac3d8958c6c6e0452c626
dpnraju95 <dpnraju95@gmail.com>
2021-12-21 12:19:50 +0530

    final Changes

--- Assignment-3/code/src/App.js
+++ Assignment-3/code/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Profile from './Profile';
 
 
-var arrow="<"
+var arrow="<";
+var arrow2=">";
+
+const profile_details={
+  "Name":"Tamanna Garg",
+  "EmployeeID":"00002512",
+  "Appointment":"9:00(24-05-2021)",
+  "Email":"tamanna@gmail.com",
+  "Phone":"9876543210"
+}
+const order_detail={
+  "Status": "In Progress",
+  "Door" : "Mark",
+  "Time":"10:30(25-05-2021)",
+}
+
 function App() {
   return (
     <div className="site-container">
-    {/* Start Your code here */}
+    <Profile employee={profile_details} order={order_detail} sign={arrow} sign2={arrow2} />
     </div>
   );
 }
